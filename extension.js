477c92b697b7f92a5b6e299557112dd70f13b924
Fabrizio <fabrizio.nastri@flexup.org>
2023-10-02 19:08:37 +0200

    imported filter script

--- extension.js
+++ extension.js
@@ -1,8 +1,8 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-const vscode = require("vscode");
-const fs = require("fs");
-const path = require("path");
+const vscode = require('vscode')
+const fs = require('fs')
+const path = require('path')
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -13,9 +13,7 @@ const path = require("path");
 function activate(context) {
   // Use the console to output diagnostic information (console.log) and errors (console.error)
   // This line of code will only be executed once when your extension is activated
-  console.log(
-    'Congratulations, your extension "vscode-project-structure" is now active!'
-  );
+  console.log('Congratulations, your extension "vscode-project-structure" is now active!')
 
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with  registerCommand
@@ -27,145 +25,148 @@ function activate(context) {
   // 	vscode.window.showInformationMessage('Hello World from vscode-project-structure!');
   // });
 
-  let disposable = vscode.commands.registerCommand(
-    "extension.generateProjectStructure",
-    () => {
-      generateProjectStructure();
-    }
-  );
+  let disposable = vscode.commands.registerCommand('extension.generateProjectStructure', () => {
+    generateProjectStructure()
+  })
 
-  context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable)
 }
 
 function getConfig() {
-    return vscode.workspace.getConfiguration('vscodeProjectStructure');
-  }
-  
+  return vscode.workspace.getConfiguration('vscodeProjectStructure')
+}
 
 function generateProjectStructure() {
-  const rootPath = vscode.workspace.workspaceFolders
-    ? vscode.workspace.workspaceFolders[0].uri.fsPath
-    : undefined;
+  const rootPath = vscode.workspace.workspaceFolders ? vscode.workspace.workspaceFolders[0].uri.fsPath : undefined
 
   if (!rootPath) {
-    vscode.window.showErrorMessage("No workspace folder is open");
-    return;
+    vscode.window.showErrorMessage('No workspace folder is open')
+    return
   }
 
-  const ignoreFilePath = path.join(rootPath, ".project_structure_ignore");
-  let ignoreFiles = [];
+  // Defines the path to the output folder
+  const outputFolderName = vscode.workspace.getConfiguration('vscodeProjectStructure').get('outputFolderPath')
+  const outputFolderPath = path.join(rootPath, outputFolderName)
 
+  // Defines the path to the ignore file within the docs folder
+  const ignoreFilePath = path.join(outputFolderPath, '.project_structure_ignore')
+  const filterFilePath = path.join(outputFolderPath, '.project_structure_filter')
+
+  // Creates the output folder if it doesn't exist
+  if (!fs.existsSync(outputFolderPath)) {
+    fs.mkdirSync(outputFolderPath)
+  }
+
+  let ignoreFiles = []
+  let filterFiles = []
+
+  // Extract list of patters for files to ignore
   if (fs.existsSync(ignoreFilePath)) {
-    const ignoreFileContent = fs.readFileSync(ignoreFilePath, "utf-8");
-    ignoreFiles = ignoreFileContent
-      .split("\n")
-      .filter((line) => line.trim() !== "");
+    const ignoreFileContent = fs.readFileSync(ignoreFilePath, 'utf-8')
+    ignoreFiles = ignoreFileContent.split('\n').filter(line => line.trim() !== '')
   }
 
+  // Merge ignore file patterns with those from .gitignore file if this option is enabled
   if (getConfig().useGitIgnore) {
-    const gitIgnorePath = path.join(rootPath, ".gitignore");
+    const gitIgnorePath = path.join(rootPath, '.gitignore')
     if (fs.existsSync(gitIgnorePath)) {
-      const gitIgnoreContent = fs.readFileSync(gitIgnorePath, "utf-8");
-      const gitIgnorePatterns = gitIgnoreContent
-        .split("\n")
-        .filter((line) => line.trim() !== "");
-      ignoreFiles = [...ignoreFiles, ...gitIgnorePatterns];
+      const gitIgnoreContent = fs.readFileSync(gitIgnorePath, 'utf-8')
+      const gitIgnorePatterns = gitIgnoreContent.split('\n').filter(line => line.trim() !== '')
+      ignoreFiles = [...ignoreFiles, ...gitIgnorePatterns]
     }
   }
 
-  let output = "";
-  output += "--- Folder Structure ---\n";
-  output += getFolderStructure(rootPath, ignoreFiles, 0);
+  let output = ''
+  output += '--- Folder Structure ---\n'
+  output += getFolderStructure(rootPath, ignoreFiles, 0)
 
-  output += "\n--- File Contents ---\n";
-  output += getFileContents(rootPath, ignoreFiles);
+  output += '\n--- File Contents ---\n'
+  output += getFileContents(rootPath, ignoreFiles)
 
-  const outputPath = path.join(rootPath, "project_structure.txt");
-  fs.writeFileSync(outputPath, output);
+  const outputPath = path.join(outputFolderPath, 'project_structure.txt')
+  fs.writeFileSync(outputPath, output)
 
-  vscode.window.showInformationMessage(
-    "Project structure generated successfully"
-  );
+  vscode.window.showInformationMessage('Project structure generated successfully')
 }
 
 function getFolderStructure(dir, ignoreFiles, level) {
-  let output = "";
+  let output = ''
 
   if (fs.existsSync(dir)) {
-    const files = fs.readdirSync(dir);
+    const files = fs.readdirSync(dir)
     files.forEach((file, index) => {
-      const fullPath = path.join(dir, file);
-      const relativePath = path.relative(process.cwd(), fullPath);
+      const fullPath = path.join(dir, file)
+      const relativePath = path.relative(process.cwd(), fullPath)
 
       if (shouldIgnore(relativePath, ignoreFiles)) {
-        return;
+        return
       }
 
-      const isLastFile = index === files.length - 1;
-      const prefix = level === 0 ? "" : isLastFile ? "└── " : "├── ";
-      const indent = " ".repeat(level * 4) + (level > 0 ? prefix : "");
+      const isLastFile = index === files.length - 1
+      const prefix = level === 0 ? '' : isLastFile ? '└── ' : '├── '
+      const indent = ' '.repeat(level * 4) + (level > 0 ? prefix : '')
 
       if (fs.lstatSync(fullPath).isDirectory()) {
-        output += indent + `[${file}]\n`;
-        output += getFolderStructure(fullPath, ignoreFiles, level + 1);
+        output += indent + `[${file}]\n`
+        output += getFolderStructure(fullPath, ignoreFiles, level + 1)
       } else {
-        output += indent + file + "\n";
+        output += indent + file + '\n'
       }
-    });
+    })
   }
 
-  return output;
+  return output
 }
 
-function getFileContents(dir, ignoreFiles) {
-  let output = "";
+function getFileContents(dir, ignoreFiles, filterFiles) {
+  let output = ''
 
-  const readDirectory = (directory) => {
-    let results = [];
-    const files = fs.readdirSync(directory);
+  const readDirectory = directory => {
+    let results = []
+    const files = fs.readdirSync(directory)
 
-    files.forEach((file) => {
-      const fullPath = path.join(directory, file);
-      const relativePath = path.relative(process.cwd(), fullPath);
+    files.forEach(file => {
+      const fullPath = path.join(directory, file)
+      const relativePath = path.relative(process.cwd(), fullPath)
 
       if (shouldIgnore(relativePath, ignoreFiles)) {
-        return;
+        return
       }
 
       if (fs.lstatSync(fullPath).isDirectory()) {
-        results = results.concat(readDirectory(fullPath));
+        results = results.concat(readDirectory(fullPath))
       } else {
-        results.push(fullPath);
+        results.push(fullPath)
       }
-    });
+    })
 
-    return results;
-  };
+    return results
+  }
 
-  const filePaths = readDirectory(dir);
+  const filePaths = readDirectory(dir)
 
-  filePaths.forEach((filePath) => {
-    const relativePath = path.relative(dir, filePath);
-    output += `\n--- File: ${relativePath} ---\n`;
-    output += fs.readFileSync(filePath, "utf-8") + "\n";
-  });
+  filePaths.forEach(filePath => {
+    const relativePath = path.relative(dir, filePath)
+    output += `\n--- File: ${relativePath} ---\n`
+    output += fs.readFileSync(filePath, 'utf-8') + '\n'
+  })
 
-  return output;
+  return output
 }
 
 function shouldIgnore(relativePath, ignorePatterns) {
   for (const pattern of ignorePatterns) {
-    if (pattern.startsWith("*")) {
-      const fileExtension = path.extname(relativePath);
+    if (pattern.startsWith('*')) {
+      const fileExtension = path.extname(relativePath)
       if (fileExtension === pattern.slice(1)) {
-        return true;
+        return true
       }
     } else if (relativePath.includes(pattern)) {
-      return true;
+      return true
     }
   }
 
-  return false;
+  return false
 }
 
 // This method is called when your extension is deactivated
@@ -173,5 +174,5 @@ function deactivate() {}
 
 module.exports = {
   activate,
-  deactivate,
-};
+  deactivate
+}
