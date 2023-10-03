646155923f6571e575186f88e451d7ab63a3aaf9
Fabrizio <fabrizio.nastri@flexup.org>
2023-10-03 15:43:50 +0200

    added options to filter & change output folder

--- extension.js
+++ extension.js
@@ -15,6 +15,8 @@ function activate(context) {
   // This line of code will only be executed once when your extension is activated
   console.log('Congratulations, your extension "vscode-project-structure" is now active!')
 
+  logStuff()
+
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with  registerCommand
   // The commandId parameter must match the command field in package.json
@@ -26,48 +28,68 @@ function activate(context) {
   // });
 
   let disposable = vscode.commands.registerCommand('extension.generateProjectStructure', () => {
-    generateProjectStructure()
+    generateProjectStructure(false)
   })
 
   context.subscriptions.push(disposable)
+
+  let disposable2 = vscode.commands.registerCommand('extension.generateFilteredProjectStructure', () => {
+    generateProjectStructure(true)
+  })
+
+  context.subscriptions.push(disposable2)
 }
 
-function getConfig() {
-  return vscode.workspace.getConfiguration('vscodeProjectStructure')
-}
-
-function generateProjectStructure() {
+function generateProjectStructure(applyFilter = false) {
+  // Defines the path to the root folder of the workspace, displays an error message if no workspace is open
+  console.log('was filter applied?', applyFilter)
   const rootPath = vscode.workspace.workspaceFolders ? vscode.workspace.workspaceFolders[0].uri.fsPath : undefined
-
   if (!rootPath) {
-    vscode.window.showErrorMessage('No workspace folder is open')
+    vscode.window.showErrorMessage('Fab! No workspace folder is open')
     return
   }
 
-  // Defines the path to the output folder
-  const outputFolderName = vscode.workspace.getConfiguration('vscodeProjectStructure').get('outputFolderPath')
-  const outputFolderPath = path.join(rootPath, outputFolderName)
+  // Defines the configuration object
+  const config = vscode.workspace.getConfiguration('vscodeProjectStructure')
 
-  // Defines the path to the ignore file within the docs folder
-  const ignoreFilePath = path.join(outputFolderPath, '.project_structure_ignore')
-  const filterFilePath = path.join(outputFolderPath, '.project_structure_filter')
+  // Defines the path to the output folder, if not defined set it to "docs"
+  const outputFolderName = config.get('outputFolderPath') || 'docs'
+  const outputFolderPath = path.join(rootPath, outputFolderName)
 
   // Creates the output folder if it doesn't exist
   if (!fs.existsSync(outputFolderPath)) {
     fs.mkdirSync(outputFolderPath)
   }
 
+  // Defines the path to the ignore file within the docs folder
+  const ignoreFilePath = path.join(outputFolderPath, '.project_structure_ignore')
+  const filterFilePath = path.join(outputFolderPath, '.project_structure_filter')
+
+  // Initialize the ignoreFiles and filterFiles arrays
   let ignoreFiles = []
   let filterFiles = []
+  let outputPath = ''
 
-  // Extract list of patters for files to ignore
+  // Check if the filter option is enabled and if the filter file exists
+  if (applyFilter && fs.existsSync(filterFilePath)) {
+    // set the output path to project_structure_filtered.txt
+    outputPath = path.join(outputFolderPath, 'project_structure_filtered.txt')
+    // Extract list of patterns for files to filter
+    const ignoreFileContent = fs.readFileSync(filterFilePath, 'utf-8')
+    filterFiles = ignoreFileContent.split('\n').filter(line => line.trim() !== '')
+  } else {
+    // iset the output path to project_structure.txt
+    outputPath = path.join(outputFolderPath, 'project_structure.txt')
+  }
+
+  // Extract list of patterns for files to ignore
   if (fs.existsSync(ignoreFilePath)) {
     const ignoreFileContent = fs.readFileSync(ignoreFilePath, 'utf-8')
     ignoreFiles = ignoreFileContent.split('\n').filter(line => line.trim() !== '')
   }
 
   // Merge ignore file patterns with those from .gitignore file if this option is enabled
-  if (getConfig().useGitIgnore) {
+  if (config.useGitIgnore) {
     const gitIgnorePath = path.join(rootPath, '.gitignore')
     if (fs.existsSync(gitIgnorePath)) {
       const gitIgnoreContent = fs.readFileSync(gitIgnorePath, 'utf-8')
@@ -79,14 +101,16 @@ function generateProjectStructure() {
   let output = ''
   output += '--- Folder Structure ---\n'
   output += getFolderStructure(rootPath, ignoreFiles, 0)
-
   output += '\n--- File Contents ---\n'
-  output += getFileContents(rootPath, ignoreFiles)
+  output += getFileContents(rootPath, ignoreFiles, filterFiles, applyFilter)
 
-  const outputPath = path.join(outputFolderPath, 'project_structure.txt')
   fs.writeFileSync(outputPath, output)
 
-  vscode.window.showInformationMessage('Project structure generated successfully')
+  vscode.window.showInformationMessage(
+    `Project structure generated successfully for ${applyFilter ? 'filtered ' : 'all'} files: 'project_structure${
+      applyFilter ? '_filtered ' : ''
+    }.txt'`
+  )
 }
 
 function getFolderStructure(dir, ignoreFiles, level) {
@@ -118,7 +142,7 @@ function getFolderStructure(dir, ignoreFiles, level) {
   return output
 }
 
-function getFileContents(dir, ignoreFiles, filterFiles) {
+function getFileContents(dir, ignoreFiles, filterFiles, applyFilter) {
   let output = ''
 
   const readDirectory = directory => {
@@ -129,14 +153,20 @@ function getFileContents(dir, ignoreFiles, filterFiles) {
       const fullPath = path.join(directory, file)
       const relativePath = path.relative(process.cwd(), fullPath)
 
+      // Ignore files that match the ignore patterns
       if (shouldIgnore(relativePath, ignoreFiles)) {
         return
       }
 
-      if (fs.lstatSync(fullPath).isDirectory()) {
-        results = results.concat(readDirectory(fullPath))
-      } else {
-        results.push(fullPath)
+      // If the file matches the filter patterns, add it to the results array
+      if (!applyFilter || shouldInclude(relativePath, filterFiles)) {
+        console.log('relativePath', relativePath, ' → → ', shouldInclude(relativePath, filterFiles))
+
+        if (fs.lstatSync(fullPath).isDirectory()) {
+          results = results.concat(readDirectory(fullPath))
+        } else {
+          results.push(fullPath)
+        }
       }
     })
 
@@ -154,8 +184,16 @@ function getFileContents(dir, ignoreFiles, filterFiles) {
   return output
 }
 
-function shouldIgnore(relativePath, ignorePatterns) {
-  for (const pattern of ignorePatterns) {
+function shouldIgnore(relativePath, ignoreFiles) {
+  return matchesPattern(relativePath, ignoreFiles)
+}
+
+function shouldInclude(relativePath, filterFiles) {
+  return matchesPattern(relativePath, filterFiles)
+}
+
+function matchesPattern(relativePath, listOfPatterns) {
+  for (const pattern of listOfPatterns) {
     if (pattern.startsWith('*')) {
       const fileExtension = path.extname(relativePath)
       if (fileExtension === pattern.slice(1)) {
@@ -175,4 +213,34 @@ function deactivate() {}
 module.exports = {
   activate,
   deactivate
+}
+
+const logStuff = () => {
+  const config = vscode.workspace.getConfiguration('vscodeProjectStructure')
+  console.log('config', config)
+
+  const workspaceFolders = vscode.workspace.workspaceFolders
+  console.log('workspaceFolders', workspaceFolders)
+
+  const fsPath0 = vscode.workspace.workspaceFolders[0].uri.fsPath
+  console.log('fsPath0', fsPath0)
+
+  const rootPath = vscode.workspace.workspaceFolders ? vscode.workspace.workspaceFolders[0].uri.fsPath : undefined
+  console.log('rootPath', rootPath)
+
+  const outputFolderName = config.get('outputFolderPath')
+  console.log('outputFolderName', outputFolderName) // docs
+
+  const outputFolderPath = path.join(rootPath, outputFolderName)
+  console.log('outputFolderPath', outputFolderPath) // c:\Users\fabri\Repos\_samples\shared-eslint-main\docs
+
+  // Defines the path to the ignore file within the docs folder
+  const ignoreFilePath = path.join(outputFolderPath, '.project_structure_ignore')
+  console.log('ignoreFilePath', ignoreFilePath) // c:\Users\fabri\Repos\_samples\shared-eslint-main\docs\.project_structure_ignore
+
+  const filterFilePath = path.join(outputFolderPath, '.project_structure_filter')
+  console.log('filterFilePath', filterFilePath) // c:\Users\fabri\Repos\_samples\shared-eslint-main\docs\.project_structure_filter
+
+  const files = fs.readdirSync(rootPath)
+  console.log('files', files)
 }
